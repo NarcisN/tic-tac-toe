bb79ffbe2c939cc39c4f1529bd2cc616b896005f
NarcisN <narcisn650@gmail.com>
2024-10-01 20:10:13 +0300

    final

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -1,32 +1,65 @@
-import React from "react";
+import React, {useState} from "react";
 import "./TicTacToe.css";
-import circle_icon from "../Assets/circle.png";
-import cross_icon from "../Assets/cross.png";
+import circle_icon from "../assets/circle.png";
+import cross_icon from "../assets/cross.png";
+import {useRef} from "react";
 
 let data = ["", "", "", "", "", "", "", "", ""];
 
 const TicTacToe = () => {
   let [count, setCount] = useState(0);
   let [lock, setLock] = useState(false);
-  const toggle = (w, num) => {
+  let titleRef = useRef(null);
+  const toggle = (e, num) => {
     if (lock) {
       return 0;
     }
-    if(count%2===0){
-      e.target.innerHTML = `<img src= `${cross_icon}`>` ;
-      data[num] ='x';
+    if (count % 2 === 0) {
+      e.target.innerHTML = `<img src= '${cross_icon}''>`;
+      data[num] = "x";
+      setCount(++count);
+    } else {
+      e.target.innerHTML = `<img src='${circle_icon}'>`;
+      data[num] = "o";
       setCount(++count);
     }
-    else{
-      e.target.innerHTML= `<img src=`${circle_icon}`>`;
-      data[num]='o';
-      setCount(++count);
+    checkWin();
+  };
+
+  const checkWin = () => {
+    if (data[0] === data[1] && data[1] === data[2] && data[2] !== "") {
+      won(data[2]);
+    } else if (data[3] === data[4] && data[4] === data[5] && data[5] !== "") {
+      won(data[5]);
+    } else if (data[6] === data[7] && data[7] === data[8] && data[8] !== "") {
+      won(data[8]);
+    } else if (data[0] === data[3] && data[3] === data[6] && data[6] !== "") {
+      won(data[6]);
+    } else if (data[1] === data[4] && data[4] === data[7] && data[7] !== "") {
+      won(data[7]);
+    } else if (data[2] === data[5] && data[5] === data[8] && data[8] !== "") {
+      won(data[8]);
+    } else if (data[0] === data[4] && data[4] === data[8] && data[8] !== "") {
+      won(data[8]);
+    } else if (data[0] === data[1] && data[1] === data[2] && data[2] !== "") {
+      won(data[2]);
+    } else if (data[2] === data[4] && data[4] === data[6] && data[6] !== "") {
+      won(data[6]);
+    }
+  };
+
+  const won = (winner) => {
+    setLock(true);
+    if (winner === "x") {
+      titleRef.current.innerHTML = `Congratulations:<img src=${cross_icon}> Wins`;
+    } else if (winner === "o") {
+      titleRef.current.innerHTML = `Congratulations:<img src=${circle_icon}> Wins`;
     }
   };
 
   return (
     <div className="container">
-      <h1 className="title">
+      <h1 className="title" ref={titleRef}>
         Tic Tac Toe Game In <span>React</span>
       </h1>
       <div className="board">
